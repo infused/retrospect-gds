c262979887b4f8ef6f59f1e3c3396beba18ea81f
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-01-04 01:04:42 +0000

    - Added phpdoc headers to administration files
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@18 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/administration/user_list.php
+++ retrospect/administration/user_list.php
@@ -1,3 +1,27 @@
+<?php
+/**
+ * Admin - User List Module
+ *
+ * @copyright 	Keith Morrison, Infused Solutions	2001-2004
+ * @author			Keith Morrison <keithm@infused-solutions.com>
+ * @package 		administration
+ * @license 		http://opensource.org/licenses/gpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License contained in the file GNU.txt for
+ * more details.
+ *
+ * $Id$
+ *
+ */  
+?>
 <link href="styles.css" rel="stylesheet" type="text/css">
 <table width="100%"  border="0" cellspacing="5" cellpadding="0">
 	<tr>
