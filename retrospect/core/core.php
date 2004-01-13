8f386b1a797909d28c11da5946d44821bf49a782
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-01-13 22:12:20 +0000

    - Added g_db_prefix var - Moved table global vars to core.php
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@41 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/core/core.php
+++ retrospect/core/core.php
@@ -37,7 +37,10 @@
  * $Id$
  *
  */
-	
+
+	# Turn on error reporting
+	error_reporting(E_ALL);	
+
 	# Create clean content and menu vars
 	$g_menu = '';
 	$g_custom_menu = '';
@@ -49,9 +52,6 @@
 	*/
 	require_once(CORE_PATH.'f_datetime.php');
 	$start =  time_get_micro();
-	
-	# Turn on error reporting
-	error_reporting(E_ALL);
 
 	# Start output buffering 
 	ob_start();
@@ -66,6 +66,99 @@
 	* it will be used instead of the default file (config.php).
 	*/
 	require_once( file_exists(CORE_PATH.'config-dev.php') ? CORE_PATH.'config-dev.php' : CORE_PATH.'config.php');
+
+	/** 
+	* Location of FPDF font definitions
+	* @global string
+	*/
+	define('FPDF_FONTPATH', LIB_PATH.'fpdf/font/');
+	
+	/**
+	* Location of media files
+	* @global string
+	*/
+	define('_MEDIA_FILES','media/files/');
+	
+	/**
+	* Location of media thumbnails
+	* @global string
+	*/
+	define('_MEDIA_THUMBS','media/thumbs/');
+	
+	# Initialize table vars
+	
+	/**
+	* Individual table.
+	* @global string $g_tbl_indiv
+	*/
+	$g_tbl_indiv  			= $g_db_prefix . 'indiv';
+	
+	/**
+	* Fact table.
+	* @global string $g_tbl_fact
+	*/
+	$g_tbl_fact   			= $g_db_prefix . 'fact';
+	
+	/**
+	* Family table.
+	* @global string $g_tbl_family
+	*/
+	$g_tbl_family 			= $g_db_prefix . 'family';
+	
+	/** 
+	* Relation table.
+	* @global string $g_tbl_relation
+	*/
+	$g_tbl_relation 		= $g_db_prefix . 'relation';
+	
+	/**
+	*
+	* Citation table.
+	* @global string $g_tbl_citation
+	*/
+	$g_tbl_citation 		= $g_db_prefix . 'citation';
+	
+	/**
+	* Source table.
+	* @global string $g_tbl_source
+	*/
+	$g_tbl_source   		= $g_db_prefix . 'source';
+	
+	/**
+	* Note table.
+	* @global string $g_tbl_note
+	*/
+	$g_tbl_note					= $g_db_prefix . 'note';
+	
+	/**
+	* Child table.
+	* @global string $g_tbl_child
+	*/
+	$g_tbl_child				= $g_db_prefix . 'children';
+	
+	/**
+	* User table.
+	* @global string $g_tbl_user
+	* */
+	$g_tbl_user					= $g_db_prefix . 'user';
+	
+	/**
+	* Language table.
+	* @global string $g_tbl_lang
+	*/
+	$g_tbl_lang					= $g_db_prefix . 'language';
+	
+	/**
+	* Options table.
+	* @global string $g_tbl_option
+	*/
+	$g_tbl_option 			= 'rgds_options';
+	
+	/**
+	* Media table.
+	* @global string $g_tbl_media
+	*/
+	$g_tbl_media				= $g_db_prefix  . 'media';
 
 	/** 
 	* Require PHP compatibility functions
