82a6a101a59dc9231df72975d2b916bfc889a7b4
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-01-19 20:52:57 +0000

    - Renamed config.php to config-dist.php
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@67 388aa38b-ea0e-0410-9b20-fec1a207d506

---
+++ retrospect/core/config-dist.php
@@ -0,0 +1,87 @@
+<?php 
+/**
+ * Contains global configuration variables.
+ *
+ * This is the first required file loaded by the core. It contains all the
+ * necessary information to establish a database connection, sets global table
+ * names, and sets global page names.
+ *
+ * @copyright 	Keith Morrison, Infused Solutions	2001-2004
+ * @author			Keith Morrison <keithm@infused-solutions.com>
+ * @package 		config
+ * @license http://opensource.org/licenses/gpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License contained in the file GNU.txt for
+ * more details.
+ *
+ * $Id$
+ *
+ */
+
+# --- Database vars ------------
+
+/**
+* Database type.
+* One of database types supported by adodb, such as (but, not limited to)
+* 'mysql', 'mssql', 'db2', 'odbc'
+*/
+$g_db_type = 'mysql';
+
+/**
+* Database hostname.
+* This is usually set to 'localhost' but could be remote hostname or ip address
+* @global string $g_db_host
+*/
+$g_db_host = 'localhost';
+
+/**
+* Database port.
+* The default MySQL installation uses port 3306
+* @global integer $g_db_port
+*/
+$g_db_port = '';
+
+/**
+* Database name.
+* The name of the database to connect to
+* @global string $g_db_name
+*/
+$g_db_name   = 'database';
+
+/**
+* Database table prefix.
+* An underscore will automatically be appended to the prefix
+* @global string $g_db_prefix
+*/
+$g_db_prefix = 'rgds_';
+
+/** 
+* Database username.
+* Username authorized to login to the database specified in {@link $g_db}
+* @global string $g_db_user
+*/
+$g_db_user = 'username';
+
+/** 
+* Database password.
+* Password for {@link $g_db_user} authorized to login to the database specified in {@link $g_db}
+* @global string $g_db_pass
+*/
+$g_db_pass = 'password';
+
+/** 
+* Default theme.
+* Specifies the default theme for page display
+* @global string $g_theme
+*/
+$g_theme	= 'default';
+
+?>
