33e8fa9e2de3da242fece4746f2e9c2f672e2312
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-01-28 23:58:06 +0000

    - changed logo file
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@87 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/administration/admin.php
+++ retrospect/administration/admin.php
@@ -33,11 +33,7 @@
 <body>
 <table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
-    <td height="50"><table border="0" cellspacing="0" cellpadding="5">
-      <tr>
-        <td><img src="images/logo.png" width="400" height="49" /></td>
-      </tr>
-    </table>    </td>
+    <td height="50" bgcolor="#0066CC"><img src="images/logo.gif" width="600" height="59" /></td>
   </tr>
   <tr>
     <td height="20"><table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#CCCCCC">
