56b549d4f721c8b35dc2cf25474deed687db311a
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-02-21 00:33:38 +0000

    - moved global var defs to separate file
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@110 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/core/core.php
+++ retrospect/core/core.php
@@ -56,93 +56,11 @@
 	*/
 	require_once(CORE_PATH.'config.php');
 
-	/** 
-	* Location of FPDF font definitions
-	* @global string
-	*/
-	define('FPDF_FONTPATH', LIB_PATH.'fpdf/font/');
-	
 	/**
-	* Location of media files
-	* @global string
+	* Load global variables
 	*/
-	define('_MEDIA_FILES','media/files/');
+	require_once(CORE_PATH.'globals.php');
 	
-	/**
-	* Location of media thumbnails
-	* @global string
-	*/
-	define('_MEDIA_THUMBS','media/thumbs/');
-	
-	# Initialize table vars
-	
-	/**
-	* Individual table.
-	* @global string $g_tbl_indiv
-	*/
-	$g_tbl_indiv  			= $g_db_prefix . 'indiv';
-	
-	/**
-	* Fact table.
-	* @global string $g_tbl_fact
-	*/
-	$g_tbl_fact   			= $g_db_prefix . 'fact';
-	
-	/**
-	* Family table.
-	* @global string $g_tbl_family
-	*/
-	$g_tbl_family 			= $g_db_prefix . 'family';
-	
-	/**
-	*
-	* Citation table.
-	* @global string $g_tbl_citation
-	*/
-	$g_tbl_citation 		= $g_db_prefix . 'citation';
-	
-	/**
-	* Source table.
-	* @global string $g_tbl_source
-	*/
-	$g_tbl_source   		= $g_db_prefix . 'source';
-	
-	/**
-	* Note table.
-	* @global string $g_tbl_note
-	*/
-	$g_tbl_note					= $g_db_prefix . 'note';
-	
-	/**
-	* Child table.
-	* @global string $g_tbl_child
-	*/
-	$g_tbl_child				= $g_db_prefix . 'children';
-	
-	/**
-	* User table.
-	* @global string $g_tbl_user
-	* */
-	$g_tbl_user					= $g_db_prefix . 'user';
-	
-	/**
-	* Language table.
-	* @global string $g_tbl_lang
-	*/
-	$g_tbl_lang					= $g_db_prefix . 'language';
-	
-	/**
-	* Options table.
-	* @global string $g_tbl_option
-	*/
-	$g_tbl_option 			= $g_db_prefix . 'options';
-	
-	/**
-	* Media table.
-	* @global string $g_tbl_media
-	*/
-	$g_tbl_media				= $g_db_prefix  . 'media';
-
 	/** 
 	* Require PHP compatibility functions
 	*/
