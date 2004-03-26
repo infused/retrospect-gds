d83f60f7c1e758152e093634ab5f00600deff216
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-03-26 01:01:31 +0000

    - adding gedcom import module
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@123 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/administration/gedcom_import.php
+++ retrospect/administration/gedcom_import.php
@@ -23,21 +23,25 @@
  */  
 ?>
 <link href="styles.css" rel="stylesheet" type="text/css">
-<form action="" method="post" enctype="multipart/form-data" name="config_form">
+<form action="<?php echo CURRENT_PAGE.'2'; ?>" method="post" enctype="multipart/form-data" name="gedcom_import_form" id="gedcom_import_form">
 <table width="100%"  border="0" cellpadding="0" cellspacing="5"> 
   <tr> 
     <td align="left" valign="top">&nbsp;</td> 
-  </tr> 
-	<?php 
-		if (isset($_POST['Import']) and $_POST['Import'] == 'Import') {
-			// Do something here
-		}
-	?>
+  </tr>
   <tr> 
     <td align="left" valign="top" class="content-subtitle"><?php echo _("Import Gedcom"); ?></td> 
   </tr> 
   <tr> 
     <td align="left" valign="top"> <table width="100%"  border="0" cellpadding="2" cellspacing="0" bgcolor="#CCCCCC"> 
+        <tr>
+          <td colspan="2" valign="middle" class="content-label">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2" valign="middle" class="content-label">&nbsp;</td>
+          </tr>
+        <tr>
+          <td colspan="2" valign="middle" class="content-label">Upload Max File Size: <?php echo ini_get('upload_max_filesize'); ?></td>
+        </tr>
         <tr> 
           <td width="125" valign="middle" class="content-label"><?php echo _("Gedcom File"); ?>:</td> 
           <td valign="middle"><input name="file" type="file" class="textbox"></td> 
@@ -48,8 +52,9 @@
     <td align="left" valign="top">&nbsp;</td> 
   </tr> 
 	<tr>
-	<td><input name="Import" type="submit" class="text" id="Import" value="<?php echo _("Import"); ?>"> 
-	<input name="<?php echo _("Reset"); ?>" type="reset" class="text" id="<?php echo _("Reset"); ?>" value="<?php echo _("Reset"); ?>"></td>
+	<td>
+		<input name="Import" type="submit" class="text" id="Import" value="<?php echo _("Import"); ?>"> 
+		<input name="<?php echo _("Reset"); ?>" type="reset" class="text" id="<?php echo _("Reset"); ?>" value="<?php echo _("Reset"); ?>"></td>
 	</tr>
 </table> 
 </form>
