a72129c8e268d93c093b0501a2e4d418b83f829d
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-06-08 23:55:50 +0000

    - adding gedcom date parser class
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@209 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/core/date.class.php
+++ retrospect/core/date.class.php
@@ -24,10 +24,11 @@
 	
 	# Define regular expressions
 	# DateParser structures
-	define('REG_DATE_GREG1','/^([0-9]{3,4}\/[0-9]{2}|[0-9]{3,4})/');
-	define('REG_DATE_GREG2','/^(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) ([0-9]{4}\/[0-9]{2}|[0-9]{1,4})/');
-	define('REG_DATE_GREG3', '/^([0-9]{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) ([0-9]{4}\/[0-9]{2}|[0-9]{1,4})/');
-	define('REG_DATE_PERIOD', '/^(FROM|TO) (.+)/');
+	define('REG_DATE_GREG1','/^(ABT|CIR|BEF|AFT|FROM|TO|EST|CAL|) *([0-9]{3,4}\/[0-9]{2}|[0-9]{3,4})$/');
+	define('REG_DATE_GREG2','/^(ABT|CIR|BEF|AFT|FROM|TO|EST|CAL|) *(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) ([0-9]{4}\/[0-9]{2}|[0-9]{1,4})$/');
+	define('REG_DATE_GREG3', '/^(ABT|CIR|BEF|AFT|FROM|TO|EST|CAL|) *([0-9]{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) ([0-9]{4}\/[0-9]{2}|[0-9]{1,4})$/');
+	define('REG_DATE_PERIOD', '/^FROM (.+) TO (.+)/');
+	define('REG_DATE_RANGE', '/^BET (.+) AND (.+)/');
 	
 	# Define some aliases
 	define('REG_DATE_YEAR', REG_DATE_GREG1);
@@ -46,6 +47,8 @@
 	define('DATE_MOD_EST',  'G0');
 	define('DATE_MOD_CAL',  'H0');
 	
+	define('DATE_EMPTY', '00000000');
+	
 	$months = array(
 		'JAN'=>'01',
 		'FEB'=>'02',
@@ -58,7 +61,19 @@
 		'SEP'=>'09',
 		'OCT'=>'10',
 		'NOV'=>'11',
-		'DEC'=>'12');
+		'DEC'=>'12'
+	);
+	
+	$modifiers = array(
+		'ABT'=>DATE_MOD_ABT,
+		'CIR'=>DATE_MOD_CIR,
+		'BEF'=>DATE_MOD_BEF,
+		'AFT'=>DATE_MOD_AFT,
+		'FROM'=>DATE_MOD_FROM,
+		'TO'=>DATE_MOD_TO,
+		'EST'=>DATE_MOD_EST,
+		'CAL'=>DATE_MOD_CAL
+	);
 	
 	/**
  	* GedcomParser class
@@ -75,48 +90,60 @@
 			$this->pdate = null;
 			$this->sdate = $string;
 			# convert date string to uppercase
-			$datestr = strtoupper($string);
+			$datestr = strtoupper(trim($string));
 			# process exact dates
 			if (preg_match(REG_DATE_YEAR, $datestr, $match)) {
-				$year = $this->_get_greg1($match[1]);
-				$this->pdate = '000000'.$year.'0000000000';
+				$year = $this->_get_greg1($match[2]);
+				$modifier = $this->_get_modifier($match[1]);
+				$this->pdate = $modifier . '0000' . $year . DATE_EMPTY;
 			}
 			elseif (preg_match(REG_DATE_MOYR, $datestr, $match)) {
 				$date = $this->_get_greg2($match);
-				$this->pdate = $date.'0000000000';
+				$this->pdate = $date . DATE_EMPTY;
 			}
 			elseif (preg_match(REG_DATE_EXACT, $datestr, $match)) {
 				$date = $this->_get_greg3($match);
-				$this->pdate = $date.'0000000000';
+				$this->pdate = $date . DATE_EMPTY;
 			}
 			elseif (preg_match(REG_DATE_PERIOD, $datestr, $match)) {
 				$date = $this->_get_period($match);
 				$this->pdate = $date;
 			}
+			else {
+				$date = ':'.$datestr;
+				$this->pdate = $date;
+			}
 		}
 		
-		/**
-		* Parses an exact date in the form of: <DAY> <MONTH> <YEAR>
-		* Returns a date code for a valid date 
-		* Returns false for an invalid date
-		* @param array $preg_match [1]=>day, [2]=>month, [3]=>year
-		* @return string
-		*/
-		function _get_greg3 ($date_arr) {
-			global $months;
-			# get the day and pad to 2 digits
-			$day .= str_pad($date_arr[1], 2, '0', STR_PAD_LEFT);
-			# get the month and pad to 2 digits
-			$month = str_pad($months[$date_arr[2]], 2, '0', STR_PAD_LEFT);
-			# get the year
-			$year = $this->_get_greg1($date_arr[3]);
-			$date = '00'.$day.$month.$year;
-			if (checkdate($month, $day, $year)) {
+		function _parse_date_string ($string) {
+			# convert date string to uppercase
+			$datestr = strtoupper(trim($string));
+			# process exact dates
+			if (preg_match(REG_DATE_YEAR, $datestr, $match)) {
+				$year = $this->_get_greg1($match[2]);
+				$modifier = $this->_get_modifier($match[1]);
+				return '0000' . $year;
+			}
+			elseif (preg_match(REG_DATE_MOYR, $datestr, $match)) {
+				$date = $this->_get_greg2($match, false);
 				return $date;
 			}
-			else {
-				return false;
+			elseif (preg_match(REG_DATE_EXACT, $datestr, $match)) {
+				$date = $this->_get_greg3($match, false);
+				return $date;
 			}
+		}
+		
+		function _get_period ($date_arr) {
+			$date1 = $this->_parse_date_string($date_arr[1]);
+			$date2 = $this->_parse_date_string($date_arr[2]);
+			return DATE_MOD_FROM . $date1 . $date2;
+		}
+		
+		function _get_range ($date_arr) {
+			$date1 = $this->_parse_date_string($date_arr[1]);
+			$date2 = $this->_parse_date_string($date_arr[2]);
+			//return DATE_MOD_BET . $date1 . $date2;
 		}
 		
 		/**
@@ -143,44 +170,73 @@
 		/**
 		* Parses dates in the form of: <MONTH> <YEAR>
 		* @param array $date_arr
+		* @param bool $return_modifier whether to return the modifier prepended to the date string
 		* @return string
 		*/
-		function _get_greg2 ($date_arr) {
+		function _get_greg2 ($date_arr, $return_modifier = true) {
 			global $months;
 			$day = '00';
+			$month_str =& $date_arr[2];
+			$year_str =& $date_arr[3];
+			$modifier = ($return_modifier == true) ? $this->_get_modifier($date_arr[1]) : '';
 			# get the month and pad to 2 digits
-			$month = str_pad($months[$date_arr[1]], 2, '0', STR_PAD_LEFT);
+			$month = str_pad($months[$month_str], 2, '0', STR_PAD_LEFT);
 			# get the year
-			$year = $this->_get_greg1($date_arr[2]);
-			$date = '00'.$day.$month.$year;
+			$year = $this->_get_greg1($year_str);
+			$date = $day.$month.$year;
 			# validate date (fake the day!) 
 			if (checkdate($month, '01', $year)) {
-				return $date;
+				return $modifier . $date;
 			}
 			else {
 				return false;
 			}
 		}
-		
-		/** 
-		* Parses date period in the following forms:
-		* FROM <DATE>
-		* TO <DATE>
-		* FROM <DATE> TO <DATE>
-		* @param array $date_arr
+	
+		/**
+		* Parses an exact date in the form of: <DAY> <MONTH> <YEAR>
+		* Returns a date code for a valid date 
+		* Returns false for an invalid date
+		* @param array $preg_match [1]=>day, [2]=>month, [3]=>year
+		* @param bool $return_modifier whether to return the modifier prepended to the date string
 		* @return string
 		*/
-		function _get_period ($date_arr) {
-			$date_str =& $date_arr[2];
-			if ($date_arr[1] == 'FROM') {
-				echo $date_str;
+		function _get_greg3 ($date_arr, $return_modifier = true) {
+			global $months;
+			$day_str =& $date_arr[2];
+			$month_str =& $date_arr[3];
+			$year_str =& $date_arr[4];
+			$modifier = ($return_modifier == true) ? $this->_get_modifier($date_arr[1]) : '';
+			# get the day and pad to 2 digits
+			$day .= str_pad($day_str, 2, '0', STR_PAD_LEFT);
+			# get the month and pad to 2 digits
+			$month = str_pad($months[$month_str], 2, '0', STR_PAD_LEFT);
+			# get the year
+			$year = $this->_get_greg1($year_str);
+			$date = $day.$month.$year;
+			if (checkdate($month, $day, $year)) {
+				return $modifier . $date;
 			}
-			elseif ($date_arr[1] == 'TO') {
-				if (preg_match(REG_DATE_EXACT, $date_str, $match)) {
-					$date = $this->_get_greg3($match);
-					echo $date;
-				}
+			else {
+				return false;
 			}
 		}
+			
+		/**
+		* Returns the date modifer code
+		* @param string $string
+		* @return string
+		*/
+		function _get_modifier ($string) {
+			global $modifiers;
+			if ($string == '') {
+				$modifier = DATE_MOD_NONE;
+			}
+			else {
+				$modifier = $modifiers[$string];
+			}
+				return $modifier;
+		}
+	
 	}
 ?>
