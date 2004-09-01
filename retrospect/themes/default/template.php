21a619c7ecff1f5851db4059832f2dcb4cdb2112
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-09-01 22:51:01 +0000

    - fixed email to friend page not working
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@263 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/themes/default/template.php
+++ retrospect/themes/default/template.php
@@ -71,7 +71,7 @@ function MM_setTextOfTextfield(objName,x,newText) { //v3.0
 <div id="content">
 	<div id="utils">
 		<a href="#" onclick="MM_openBrWindow('<?php echo htmlentities((CURRENT_PAGE == $_SERVER['PHP_SELF']) ? CURRENT_PAGE.'?print=y' : CURRENT_PAGE.'&print=y'); ?>','','scrollbars=yes,resizable=yes,width=640,height=480')"><img src="themes/default/images/printbutton.gif" border="0" alt="<?php echo gtc("Print"); ?>" /></a>
-		<a href="#" onclick="MM_openBrWindow('<?php echo htmlentities(Theme::getPage($g_theme, 'email').'?'.$_SERVER['QUERY_STRING']); ?> ','','scrollbars=yes,resizable=yes,width=400,height=225')"><img src="themes/default/images/emailbutton.gif" border="0" alt="<?php echo gtc("E-mail this to a friend"); ?>" /></a>
+		<a href="#" onclick="MM_openBrWindow('<?php echo htmlentities($_SERVER['PHP_SELF'].'?option=email&print=y'); ?> ','','scrollbars=yes,resizable=yes,width=400,height=225')"><img src="themes/default/images/emailbutton.gif" border="0" alt="<?php echo gtc("E-mail this to a friend"); ?>" /></a>
 	</div>
 	<?php	echo $g_content; 	unset($g_content); ?>
 </div>
