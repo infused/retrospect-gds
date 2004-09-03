82b2bd9416d05118c93abaa92e7994e7e7b12b69
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2004-09-03 22:53:22 +0000

    - reduced to code to display database statistics
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@265 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/administration/status.php
+++ retrospect/administration/status.php
@@ -28,17 +28,20 @@
     <td align="left" valign="top">
 		<table width="100%"  border="0" cellpadding="0" cellspacing="0" class="section">
       <tr>
-        <td class="section_head">System Alerts </td>
+        <td class="section_head">System Alerts</td>
       </tr>
       <tr>
         <td class="section_body">
 				<?php 
+					/* Password Expired! */
 					if (auth::PasswordExpired($_SESSION['uid'])) { 
 						echo '<p>';
 						$here = '<a href="'.$_SERVER['PHP_SELF'].'?option=user_edit&id='.$_SESSION['uid'].'">here</a>';
 						printf('The admin password has expired.  Go %s to change it immediately!', $here);
 						echo '</p>';
 					} 
+					
+					/* Install directory exists! */
 					if (file_exists(ROOT_PATH.'/../install')) {
 						echo '<p>The install directory should be deleted immediately.</p>';
 					}
@@ -47,110 +50,33 @@
       </tr>
     </table>
     </td>
-    <td width="300" align="right"><table width="100%"  border="0" cellpadding="0" cellspacing="0" class="section">
+    <td width="300" align="right">
+			<table width="100%"  border="0" cellpadding="0" cellspacing="0" class="section">
         <tr>
-          <td class="section_head">Database Records </td>
+          <td class="section_head">Database Statistics</td>
         </tr>
         <tr>
-          <td class="section_body"><table width="300"  border="0" cellpadding="2" cellspacing="0">
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_CHILD; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_CHILD;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_CITATION; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_CITATION;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_COMMENT; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_COMMENT;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_FACT; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_FACT;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_FAMILY; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_FAMILY;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_INDIV; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_INDIV;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_LANG; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_LANG;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_MEDIA; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_MEDIA;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_NOTE; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_NOTE;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_OPTION; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_OPTION;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-            <tr bgcolor="#CCCCCC">
-              <td><?php echo TBL_SOURCE; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_SOURCE;
-										echo $db->GetOne($sql);
-				 					?>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo TBL_USER; ?></td>
-              <td><?php 
-										$sql = "SELECT COUNT(*) FROM ".TBL_USER;
-										echo $db->GetOne($sql);
-									?>
-              </td>
-            </tr>
-          </table></td>
+          <td class="section_body">
+						<table width="300"  border="0" cellpadding="2" cellspacing="0">
+							<?php
+								/* Print table record counts */
+								$tables = $db->MetaTables('TABLES');
+								$row = 1;
+								foreach ($tables as $table) {
+									$row++;
+									$sql = 'SELECT COUNT(*) FROM '.$table;
+									$count = $db->GetOne($sql);
+									if ($row % 2 == 0) { echo '<tr bgcolor="#CCCCCC">'; }
+									else { echo '<tr>'; }
+									echo '<td>'.$table.'</td>';
+									echo '<td>'.$count.'</td>';
+									echo '</tr>';
+								}
+							?>
+	          </table>
+					</td>
         </tr>
-      </table>       </td>
+      </table>
+		</td>
   </tr>
 </table>
