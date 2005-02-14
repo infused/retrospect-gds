e1e1f4131db0baea39a1bfb9461e9cd2d125496b
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2005-02-14 06:25:10 +0000

    - added installer.class.php
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@404 388aa38b-ea0e-0410-9b20-fec1a207d506

---
+++ retrospect/install/installer.class.php
@@ -0,0 +1,48 @@
+<?php
+
+class Installer {
+
+	var $inivars;
+	var $extensions;
+	var $phpversion;
+
+	function Installer() {
+		$this->inivars = ini_get_all();
+		$this->extensions = get_loaded_extensions();
+		$this->phpversion = phpversion();
+	}
+	
+	function PHPVersion() {
+		return $this->phpversion;
+	}
+	
+	function ini_SafeMode() {
+		return $this->inivars['safe_mode']['local_value'];
+	}
+
+	function ini_RegisterGlobals() {
+		return $this->inivars['register_globals']['local_value'];
+	}
+
+	function ini_FileUploads() {
+		return $this->inivars['file_uploads']['local_value'];
+	}
+
+	function ini_MagicQuotes() {	
+		return $this->inivars['magic_quotes']['local_value'];
+	}
+	
+	function ext_Gettext() {
+		return in_array('gettext', $this->extensions);
+	}
+	
+	function make_writable($filespec) {
+		if (is_writable($filespec)) return true;
+		else return chmod($filespec, 0644);
+	}
+}
+
+
+
+
+?>
