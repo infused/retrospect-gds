fdff994a7457262430a617c33c38c9a553d54f4d
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2005-04-18 23:55:03 +0000

    added coppermine gallery plugin
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@437 388aa38b-ea0e-0410-9b20-fec1a207d506

---
+++ retrospect/core/plugins/coppermine.php
@@ -0,0 +1,42 @@
+<?php
+ /* This is an experimental plugin for linking Coppermine
+	* photos to individuals.
+	* @copyright 2005 Keith Morrison
+	*/
+	
+	class GalleryPlugin {
+		
+		# Customize these variables...
+		var $db_type = 'mysql';
+		var $db_host = 'localhost';
+		var $db_port = '';
+		var $db_name = 'retrospect';
+		var $db_user = 'root';
+		var $db_pass = '';
+		var $cpg_pictures_tbl = 'cpg132_pictures';
+		var $cpg_search_url = 'http://retrospect/cpg/thumbnails.php';
+		
+		
+		# Don't touch these...
+		var $name = 'Coppermine Plugin';
+		var $db;
+		
+		function GalleryPlugin() {
+			$this->db =& AdoNewConnection($this->db_type);
+			$host = ($this->db_port != '') ? $this->db_host.':'.$this->db_port : $this->db_host;
+			$this->db->Connect($host, $this->db_user, $this->db_pass, $this->db_name);
+		}
+		
+		function media_count($id) {
+			$sql = 'SELECT COUNT(*) FROM '.$this->cpg_pictures_tbl;
+			$sql .= ' WHERE keywords like "%'.$id.'%"';
+			return $this->db->GetOne($sql);
+		}
+		
+		function media_link($id) {
+			return $this->cpg_search_url.'?album=search&type=full&search='.$id;
+		}
+		
+	}
+	
+?>
