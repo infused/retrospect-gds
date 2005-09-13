6079a1b8f97ae8024ed2f828aa9801a2933173d1
kmorrison <kmorrison@388aa38b-ea0e-0410-9b20-fec1a207d506>
2005-09-13 17:20:51 +0000

    Simplified and re-organized config file.
    git-svn-id: https://retrospect.svn.sourceforge.net/svnroot/retrospect/trunk@557 388aa38b-ea0e-0410-9b20-fec1a207d506

--- retrospect/core/config-dist.php
+++ retrospect/core/config-dist.php
@@ -1,97 +1,35 @@
 <?php 
-/**
- * Contains global configuration variables.
- *
- * This is the first required file loaded by the core. It contains all the
- * necessary information to establish a database connection, sets global table
- * names, and sets global page names.
- *
- * @copyright 	Keith Morrison, Infused Solutions	2001-2005
- * @author			Keith Morrison <keithm@infused-solutions.com>
- * @package 		config
- * @license http://opensource.org/licenses/gpl-license.php
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License contained in the file GNU.txt for
- * more details.
- */
- 
- /**
- * $Id$
- */
-
-# --- Database vars ------------
-
-/**
-* Database type.
-* One of following supported database type strings:
-* mysql, mssql, postgres7, odbc-mssql
-*/
+# Database Type
+# Valid types are: mysql, mssql, postgres7, odbc-mssql
 $g_db_type = 'mysql';
 
-/**
-* Database hostname.
-* This is usually set to 'localhost' but could be remote hostname or ip address
-* @global string $g_db_host
-*/
-$g_db_host = 'localhost';
-
-/**
-* Database port.
-* The default port that your database software uses.
-* Leave this blank if you don't know.
-* @global integer $g_db_port
-*/
-$g_db_port = '';
-
-/**
-* Database name.
-* The name of the database to connect to
-* @global string $g_db_name
-*/
+# Database Name
+# The name of the database to connect to.
 $g_db_name   = 'database';
 
-/**
-* Database table prefix.
-* Don't change this unless you really need to.
-* @global string $g_db_prefix
-*/
-$g_db_prefix = 'rgds_';
-
-/** 
-* Database username.
-* Username authorized to login to the database specified in {@link $g_db}
-* @global string $g_db_user
-*/
+# Database Username
 $g_db_user = 'username';
 
-/** 
-* Database password.
-* Password for {@link $g_db_user} authorized to login to the database specified in {@link $g_db}
-* @global string $g_db_pass
-*/
+# Database Password
 $g_db_pass = 'password';
 
-/** 
-* Default theme.
-* Specifies the default theme for page display.
-* Change this to point to alternate themes.
-* @global string $g_theme
-*/
+# Database Hostname
+# This is usually set to 'localhost' but could be remote hostname or ip address
+$g_db_host = 'localhost';
+
+# Database Port
+# The port used to connect to your database. This should usually be left blank.
+$g_db_port = '';
+
+# Database table prefix
+# Don't change this unless you know what you are doing.
+$g_db_prefix = 'rgds_';
+
+# Theme
 $g_theme	= 'summerbreeze';
 
-/** 
-* Default admin theme.
-* Specifies the default theme to use for the administration site.
-* @global string $g_admin_theme
-*/
+# Admin Theme
 $g_admin_theme	= 'default';
 
 ?>
